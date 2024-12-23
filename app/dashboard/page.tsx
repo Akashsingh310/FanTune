f9c10da09c87edbece44ff748711be4a130c2381
AKASH SINGH <52318783+Akashsingh310@users.noreply.github.com>
2024-12-23 21:33:45 +0530

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,7 +6,7 @@ import SongVotingPlatform from '../components/SongVotingPlatform'
 
 
 
-const creatorId = "ef0ea9db-00f9-42cf-a1a7-2401b5b8a5a9"
+const creatorId = "962b4598-2104-46da-a03a-be91247a37a5"
 
 export default function Components() {
   return <SongVotingPlatform creatorId = {creatorId} playVideo = {true} />
