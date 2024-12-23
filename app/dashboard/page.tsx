0e0f7c0aeb640e0f7e8c85b335c4d69771479274
“Akash <akash22652@gmail.com>
2024-12-23 22:20:51 +0530

    parametrized the creatorId

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,38 @@
-'use client'
 
 
-import 'react-lite-youtube-embed/dist/LiteYouTubeEmbed.css'
-import SongVotingPlatform from '../components/SongVotingPlatform'
+// app/dashboard/page.tsx
+import { prismaclient } from '../lib/db';
+import SongVotingPlatform from '../components/SongVotingPlatform';
+import { getServerSession } from 'next-auth/next';
 
+type Props = {
+  creatorId: string | null;
+};
 
+export default async function DashboardPage() {
+  const session = await getServerSession();
 
-const creatorId = "ef0ea9db-00f9-42cf-a1a7-2401b5b8a5a9"
+  if (!session?.user?.email) {
+    return <div>No session found.</div>; 
+  }
 
-export default function Components() {
-  return <SongVotingPlatform creatorId = {creatorId} playVideo = {true} />
+  const user = await prismaclient.user.findFirst({
+    where: {
+      email: session.user.email,
+    },
+  });
+
+  if (!user) {
+    return <div>No user found.</div>; // Handle case where user is not found
+  }
+
+  return <ClientComponent creatorId={user.id} />;
+}
+
+function ClientComponent({ creatorId }: { creatorId: string }) {
+  return (
+    <div>
+      <SongVotingPlatform creatorId={creatorId} playVideo={true} />
+    </div>
+  );
 }
